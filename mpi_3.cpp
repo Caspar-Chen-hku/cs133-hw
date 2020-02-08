fbfa47c45d06f3774640f0fa453efdb1ce257e17
Caspar-Chen-hku <susume@connect.hku.hk>
2020-02-08 15:17:48 -0600

    Update mpi_3.cpp

--- mpi_3.cpp
+++ mpi_3.cpp
@@ -15,14 +15,14 @@ int main(int argc, char *argv[])
     int *a;
     int *h;
 
+    srand (time(NULL));
+
     MPI_Init(&argc, &argv);
 
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     a = new int[N];
-
-    srand (time(NULL));
     
     for (int i=0; i<N; i++){
         a[i] = rand()%m + l;
