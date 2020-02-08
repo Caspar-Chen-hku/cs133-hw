5f876d0fed90b0caa24d939108f32976d026241a
Caspar-Chen-hku <susume@connect.hku.hk>
2020-02-08 14:33:18 -0600

    Create mpi_2.cpp

---
+++ mpi_2.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <mpi.h>
+ 
+int main(int argc, char *argv[])
+{
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    int color = world_rank % 4;
+
+    MPI_Comm mod_comm;
+    MPI_Comm_split(MPI_COMM_WORLD, color, world_rank, &mod_comm);
+
+    int row_rank, row_size;
+    MPI_Comm_rank(mod_comm, &row_rank);
+    MPI_Comm_size(mod_comm, &row_size);
+
+    printf("WORLD RANK: %d \t ROW RANK/SIZE: %d/%d\n",
+	    world_rank, row_rank, row_size);
+
+    MPI_Comm_free(&mod_comm);
+
+}
